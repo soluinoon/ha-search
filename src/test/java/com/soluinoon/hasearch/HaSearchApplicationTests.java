2c115efbee41cbf8220529364f084d17d4fb2403
eyeofsol <kingjihong@naver.com>
2023-03-06 21:19:03 +0900

    Initial commit

---
+++ src/test/java/com/soluinoon/hasearch/HaSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soluinoon.hasearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
