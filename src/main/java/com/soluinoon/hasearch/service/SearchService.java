4000e2871ed71665517e07d97e3659f5095e8d59
eyeofsol <kingjihong@naver.com>
2023-03-09 15:11:58 +0900

    fix: json to Java Obj 오류 수정
    ObjectMapper는 json을 자바 오브젝트로 파싱할 때, Getter를 이용합니다.
    따라서 SummonerResponse에 Getter를 추가했습니다.
    
    그리고 SummonerResponse를 Riot API 스펙에 맞게 SummonerDto로 변경했습니다.

--- src/main/java/com/soluinoon/hasearch/service/SearchService.java
+++ src/main/java/com/soluinoon/hasearch/service/SearchService.java
@@ -1,44 +1,31 @@
 package com.soluinoon.hasearch.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.soluinoon.hasearch.dto.search.SummonerResponse;
+import com.soluinoon.hasearch.dto.search.SummonerDto;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpHead;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
 public class SearchService {
     @Value("${external.api.key}")
     private String key;
     private ObjectMapper objectMapper = new ObjectMapper();
+    private CloseableHttpClient client = HttpClients.createDefault();
 
-    public SummonerResponse getSummonerByName(String name) {
+
+    public SummonerDto getSummonerByName(String name) {
         try {
-            CloseableHttpClient client = HttpClients.createDefault();
-
             String url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/vezimil";
             UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).queryParam("api_key", key).build(false);
-
-            System.out.println("uri.toString() = " + uri.toString());
-
             HttpGet request = new HttpGet(uri.toUri());
             HttpResponse response = client.execute(request);
 
@@ -47,7 +34,8 @@ public class SearchService {
             }
 
             HttpEntity entity = response.getEntity();
-            return objectMapper.readValue(entity.getContent(), SummonerResponse.class);
+
+            return objectMapper.readValue(entity.getContent(), SummonerDto.class);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
